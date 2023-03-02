79850eed0b96bcb32b1e8128b2a7dd29d0e9aead
heeho <114281631+heehoh@users.noreply.github.com>
2023-03-02 19:31:34 +0900

    syntax_error -ing

--- syntax_error.c
+++ syntax_error.c
@@ -6,61 +6,102 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 11:56:01 by hujeong           #+#    #+#             */
-/*   Updated: 2023/03/02 17:36:18 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/03/02 19:30:53 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "./src/libft/libft.h"
 
-int	flag_quote(char *sep_pipe, int *quote_flag)
+int	syntax_quote_error(int quote)
 {
-	int	prev_quote_flag;
+	if (quote == 1)
+		write(2,
+			"> minishell: unexpected EOF while looking for matching `''\n", 59);
+	else
+		write(2,
+			"> minishell: unexpected EOF while looking for matching `\"'\n", 59);
+	write(2, "minishell: syntax error: unexpected end of file\n", 43);
+	return (1);
+}
 
-	prev_quote_flag = *quote_flag;
-	if (*sep_pipe == '\'')
+int	syntax_redir_error(char *input)
+{
+	if (*input == '\0')
+		write(2,
+			"minishell: syntax error near unexpected token `newline'\n", 56);
+	else if (*input == '<')
 	{
-		if (*quote_flag == 0)
-			*quote_flag = 1;
-		else if (*quote_flag == 1)
-			*quote_flag = 0;
+		if (input[1] == '<')
+			write(2,
+				"minishell: syntax error near unexpected token `<<'\n", 51);
+		else
+			write(2,
+				"minishell: syntax error near unexpected token `<'\n", 50);
 	}
-	else if (*sep_pipe == '\"')
+	else
 	{
-		if (*quote_flag == 0)
-			*quote_flag = 2;
-		else if (*quote_flag == 2)
-			*quote_flag = 0;
+		if (input[1] == '>')
+			write(2,
+				"minishell: syntax error near unexpected token `>>'\n", 51);
+		else
+			write(2,
+				"minishell: syntax error near unexpected token `>'\n", 50);
 	}
-	if (prev_quote_flag != *quote_flag)
-		return (1);
+	return (1);
+}
+
+int	syntax_pipe_error(void)
+{
+	write(2,
+		"minishell: syntax error near unexpected token `|'\n", 45);
+	return (1);
+}
+
+int	syntax_pipe_redir_error(char **input, int redir_token)
+{
+	char	redir_token;
+
+	if (**input == '|')
+	{
+		++(*input);
+		while (**input == ' ' || **input == '<' || **input == '>')
+			++(*input);
+		if (*input == '\0')
+			return (syntax_pipe_error());
+	}
+	else if (**input == '<' || **input == '>')
+	{
+		redir_token = **input;
+		if ((*input)[1] == '<' || (*input)[1] == '>')
+			++(*input);
+		if (redir_token != **input)
+			return (syntax_redir_error(*input));
+		while (**input == ' ')
+			++(*input);
+		if (**input == '|' || **input == '<' || **input == '>')
+			return (syntax_redir_error(*input));
+		if (**input == '\0')
+			return (syntax_redir_error(*input));
+	}
 	return (0);
 }
 
 int	syntax_error(char *input)
 {
 	int		quote_flag;
-	int		error_flag;
-	char	redir_token;
 
 	quote_flag = 0;
-	error_flag = 0;
 	while (*input)
 	{
 		flag_quote(input, &quote_flag);
-		if (quote_flag == 0
-			&& (*input == '|' || *input == '<' || *input == '>'))
-		{
-
-		}
-		else if (quote_flag == 0 && ft_isalnum(*input) == 0)
-		{
-
-		}
+		if (quote_flag == 0)
+			if (syntax_pipe_redir_error(&input, 0))
+				return (1);
 		++input;
 	}
 	if (quote_flag != 0)
-		return(quote_error(quote_flag));
+		return (syntax_quote_error(quote_flag));
 	return (0);
 }
 
