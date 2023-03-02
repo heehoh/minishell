6a42be71df0bc0f900b1448cd35e42d3baa1b699
migo <migo@student.42.seoul.kr>
2023-03-02 17:23:05 +0900

    migo1

--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,12 +6,23 @@
 /*   By: migo <migo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 14:23:40 by migo              #+#    #+#             */
-/*   Updated: 2023/03/02 14:47:05 by migo             ###   ########.fr       */
+/*   Updated: 2023/03/02 17:16:04 by migo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../minishell.h"
+
+int	ft_isalpha(int c)
+{
+	if (65 <= c && c <= 90)
+		return (1);
+	else if (97 <= c && c <= 122)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
 
 void	read_env(t_env *env)
 {
@@ -21,6 +32,44 @@ void	read_env(t_env *env)
 		printf("%s\n", env->var);
 		env = env->next;
 	}
+}
+
+int	same_env(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=')
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+int	rule_env(char *str, t_env *env)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=' && ft_isalpha(str[0]))
+		{
+			while (env)
+			{
+				if (ft_strncmp(env->var, str, same_env(str)) == 0)
+					return (0);
+				env = env->next;
+			}
+			return (1);
+		}
+		i++;
+	}
+	if (ft_isalpha(str[0]) == 0)
+		printf("export: \'%s\': not a valid identifier\n", str);
+	return (0);
 }
 
 void	builtin_export(t_cmd *cmd)
@@ -34,17 +83,19 @@ void	builtin_export(t_cmd *cmd)
 	{
 		while (node->next)
 			node = node->next;
-		if (cmd->option[1] != NULL)
+		while (cmd->option[++i])
 		{
-			node->next = malloc(sizeof(t_env));
-			if (node->next == 0)
-				return (0);
-			node->next->var = ft_strdup(cmd->option[1]);
-			node->next->next = NULL;
+			if (rule_env(cmd->option[i], g_global) == 1)
+			{
+				node->next = malloc(sizeof(t_env));
+				if (node->next == 0)
+					return ;
+				node->next->var = ft_strdup(cmd->option[i]);
+				node->next->next = NULL;
+				node = node->next;
+			}
 		}
-		else
-		{
+		if (cmd->option[1] == NULL)
 			read_env(g_global);
-		}
 	}
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: migo <migo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 11:30:37 by migo              #+#    #+#             */
-/*   Updated: 2023/03/02 14:52:43 by migo             ###   ########.fr       */
+/*   Updated: 2023/03/02 17:14:39 by migo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "minishell.h"
-
-void	read_env(t_env *env)
-{
-	while (env)
-	{
-		printf("declare -x ");
-		printf("%s\n", env->var);
-		env = env->next;
-	}
-}
-
-void	builtin_export(t_cmd *cmd)
-{
-	int		i;
-	t_env	*node;
-
-	i = 0;
-	node = g_global;
-	if (ft_strncmp((cmd->option[0]), "export", ft_strlen(cmd->option[0])) == 0)
-	{
-		while (node->next)
-			node = node->next;
-		if (cmd->option[1] != NULL)
-		{
-			node->next = malloc(sizeof(t_env));
-			if (node->next == 0)
-				return (0);
-			node->next->var = ft_strdup(cmd->option[1]);
-			node->next->next = NULL;
-		}
-		else
-		{
-			read_env(g_global);
-		}
-	}
-}
 
 int	main(int argc, char **argv, char **env)
 {
@@ -87,7 +51,6 @@ int	main(int argc, char **argv, char **env)
 		cmd = cmd->next;
 		++j;
 		}
-		builtin_export(cmd);
 		free(input);
 	}
 }
